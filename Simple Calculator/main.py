900929351420c7d80e281c9bc9d868d48fcbb39f
bikal <106312691+imbikal@users.noreply.github.com>
2024-11-25 00:30:45 +0545

    SimpleAirthmeticCalculator

---
+++ Simple Calculator/main.py
@@ -0,0 +1,24 @@
+#Python Calculator
+
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(result)
+elif operator == "-":
+    result = num1 - num2
+    print(result)
+elif operator == "*":
+    result = num1 * num2
+    print(result)
+elif operator == "/":
+    if num2 == 0:
+        print("Invalid!!!,Denominator can't be zero")
+    else:
+        result = num1 / num2
+        print(result)
+else:
+    print("Invalid Operator,Try again")
+
