8250546d3e6cfd79b0201894f1a7476952ae8cee
bikal <106312691+imbikal@users.noreply.github.com>
2024-11-25 23:39:50 +0545

    EmailSlicer

---
+++ Email Slicer/main.py
@@ -0,0 +1,12 @@
+# Email slicer program
+
+email = input("Enter your email: ")
+
+index = email.index("@")
+
+username = email[:index]
+
+domain = email[index + 1:]
+
+print(f"Your username is {username} and domain is {domain}")
+
